1fc199dc0ffa9640534d9c9ab997c30b25bed606
RjtSahu <sahurjt@gmail.com>
2019-04-11 23:05:49 +0530

    created method for various ops in searchService

--- js/main.js
+++ js/main.js
@@ -4,35 +4,63 @@ $("#search-button").click(() => {
     initSearch(searchQuery);
 });
 
+let searchService;
+
 function initSearch(searchQuery) {
     console.log('searched :', searchQuery);
 
-    let searchService = new SearchService(searchQuery);
+    searchService = new SearchService(searchQuery);
 
     searchService.init();
 }
 
- function SearchService(searchQuery) {
+function SearchService(searchQuery) {
 
     var info = {
         currentPageIndex: 0,
         numberOfPages: 0
-    }    
+    }
     var searchText = searchQuery;
+    var results = []
 
-    this.init = async function(){
-        console.log('inside init searchText:',searchText);
+    this.init = async function () {
+        console.log('inside init searchText:', searchText);
         const questions = await getStackOverflowResults(searchText);
-        console.log('questions : ',questions);
+        console.log('questions : ', questions);
+
+        results = Object.assign({}, questions);
+
+        info.currentPageIndex = 0;
+        info.numberOfPages = results.length;
+
+        showQuestion(questions[0]);
+    }
+
+    this.showPageResult = async (data) => {
+
+    }
+
+    this.showNextPage = async () => {
+
+    }
+
+    this.showPreviousPage = async () => {
+
     }
 }
 
 SearchService.prototype.onPrevious = function () {
     console.log('onPrevious');
+    if (searchService) {
+        searchService.showPreviousPage();
+    }
 }
 
 SearchService.prototype.onNext = function () {
     console.log('onNext');
+    if (searchService) {
+        searchService.showNextPage();
+    }
 }
 
 $("#previous-button").click(SearchService.prototype.onPrevious);
