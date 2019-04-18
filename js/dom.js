37d254ce0091b55aad502b461073d9b4cf2ff5b3
RjtSahu <sahurjt@gmail.com>
2019-04-18 12:22:09 +0530

    dev and integrated ui for answer cards

--- js/dom.js
+++ js/dom.js
@@ -5,7 +5,44 @@ let DomFactory = () => {
         let el = document.getElementById('question-title');
         el.innerText = title;
     }
+    this.showQuestionContent = (questionObject)=>{
+        $('#question-content').innerHTML = questionObject.inner_html;
+    }
+
+    this.toggleBlockVisibility = () => {
+        $('#search-header').toggleClass('block-hidden');
+        $('#content-after-search').toggleClass('block-hidden');
+    }
+
+    this.showAnswers = (answers) => {
+        let answerListDiv = document.getElementById('answer-list');
+       
+        // delete all child element if any...
+        while (answerListDiv.firstChild) {
+            answerListDiv.removeChild(answerListDiv.firstChild);
+        }
+       
+        answers.forEach(answerObject => {
+            const content = prepareAnswerDiv(answerObject);
+            answerListDiv.appendChild(content);
+        });
+    }
+
+    this.prepareAnswerDiv = (answerObject) => {
+        let cardDiv = document.createElement('div');
+        cardDiv.setAttribute('class', 'card white-grey darken-1');
+
+        let cardContentDiv = document.createElement('div');
+        cardContentDiv.setAttribute('class', 'card-content black-text answer-content');
+        cardDiv.appendChild(cardContentDiv);
+
+        cardContentDiv.innerHTML = answerObject.inner_html;
+        return cardDiv;
+    }
     return {
-        showQuestionTitle
+        showQuestionTitle,
+        showQuestionContent,
+        showAnswers,
+        toggleBlockVisibility
     }
 }
